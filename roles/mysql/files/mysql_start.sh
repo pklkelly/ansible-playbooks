abafa54baeba0dfb63d6e299b7741f1510ac32ef
kelly.pan <1269812761@qq.com>
2017-03-17 11:28:28 +0800

    ok

--- roles/mysql/files/mysql_start.sh
+++ roles/mysql/files/mysql_start.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 tar  zxf /data/mariadb-10.1.17-linux-glibc_214-x86_64.tar.gz  -C /data/mysql
+cd /data/mysql
+ln -s mariadb-10.1.17-linux-glibc_214-x86_64  mysql
 chown -R mysql:mysql  /data/mysql
-su - mysql 
+#su - mysql 
 #echo PATH
 echo 'export MYSQL_HOME=/data/mysql/mysql' >> /home/mysql/.bash_profile
 echo 'export PATH=$PATH:$MYSQL_HOME/bin' >> /home/mysql/.bash_profile
-cd /data/mysql/
-ln -s mariadb-10.1.17-linux-glibc_214-x86_64  mysql
+su - mysql
+source .bash_profile
+cd /data/mysql/mysql
 #initial mysql
-cd  /data/mysql/mysql
 ./scripts/mysql_install_db --defaults-file=/data/mysql/my.cnf 
 #service mysqld
 ./bin/mysqld_safe  --defaults-file=/data/mysql/my.cnf &
+echo "aa"
--- roles/redis/files/redis_start.sh
+++ roles/redis/files/redis_start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 tar zxf /data/redis-3.2.5.tar.gz -C /data/redis/
+cd /data/redis
+ln -s redis-3.2.5 redis 
 chown -R  redis:redis /data/redis/
 su - redis       
 #echo PATH
 echo 'export REDIS_HOME=/data/redis/redis' >> /home/redis/.bash_profile
 echo 'export PATH=$PATH:$REDIS_HOME/src' >>  /home/redis/.bash_profile
+source .bash_profile
 #service redis
-cd /data/redis/
-ln -s redis-3.2.5 redis 
 cd /data/redis/redis/src
+
 ./redis-server  /data/redis/redis.conf  
